188ec488badb6408b1680508e1917b0df1013307
rainbow-ludwig <125353857+rainbow-ludwig@users.noreply.github.com>
2023-05-22 00:35:18 +0800

    Update uda_train.py

--- uda_train.py
+++ uda_train.py
@@ -69,7 +69,7 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
     callbacks.run('on_pretrain_routine_start')
 
     # Directories
-    w = save_dir / 'weights'  # weights dir
+    var_save_dir = save_dir / 'weights'  # weights dir
     (w.parent if evolve else w).mkdir(parents=True, exist_ok=True)  # make dir
     last, best = w / 'last.pt', w / 'best.pt'
 
@@ -574,8 +574,8 @@ def train(hyp, opt, device, callbacks):  # hyp is path/to/hyp.yaml or hyp dictio
                     torch.save(ckpt, best)
                 if (epoch > 0) and (opt.save_period > 0) and (epoch % opt.save_period == 0):
                     # Use str() to convert epoch to string
-                    # torch.save(ckpt, w / f'epoch{str(epoch)}.pt')
-                    torch.save(ckpt, w + f'/epoch{epoch}.pt')
+                    torch.save(ckpt, var_save_dir / f'epoch{str(epoch)}.pt')
+                    # torch.save(ckpt, var_save_dir + f'/epoch{epoch}.pt')
                 del ckpt
                 callbacks.run('on_model_save', last, epoch, final_epoch, best_fitness, fi)
 
